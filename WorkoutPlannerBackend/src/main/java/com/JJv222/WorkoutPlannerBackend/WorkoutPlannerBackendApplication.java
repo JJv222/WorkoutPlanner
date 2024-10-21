7ba12a0c40b3f6b850d666b3672777d67d3fb9f9
JJv222 <kubcat828@gmail.com>
2024-10-21 22:31:58 +0200

    first commit

---
+++ WorkoutPlannerBackend/src/main/java/com/JJv222/WorkoutPlannerBackend/WorkoutPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.JJv222.WorkoutPlannerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ WorkoutPlannerBackend/src/test/java/com/JJv222/WorkoutPlannerBackend/WorkoutPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JJv222.WorkoutPlannerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
